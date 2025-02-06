9f929905fd90572a382d5b2eabca52f4b85e90f2
321213132 <minhun5257@gmail.com>
2025-02-06 20:57:00 +0900

    feat - 몬스터 스폰
    임시 저장

---
+++ 14_TextRPG/SpawnMonster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_TextRPG
+{
+    //랜덤 몬스터 스폰
+    //1~4마리 랜덤 등장
+
+    //해야할 것
+    //몬스터 정보를 저장할수 있는 변수 필요(레벨 이름 체력 공격력)
+    
+    
+    internal class SpawnMonster
+    {
+        enum MonsterType
+        {
+            Slime,      //0
+            Skeleton,   //1
+            Goblin,     //2
+
+        }
+        public void SpawnMons(Monster monster)
+        {
+            Random random = new Random();
+            int monsNum = random.Next(1, 4);//몬스터 마리수 지정
+            int[] monsters = new int[3];//몬스터 저장 배열
+            //int에서 다른 것으로 교체
+
+
+            for (int i = 0; i <= monsNum;  i++)//몬스터 수 만큼 실행
+            {
+                int randNum = random.Next(0, 3);
+                switch (randNum)
+                {
+                    //monsters[i] 
+
+                    /*case (int)MonsterType.Slime:
+                    case (int)MonsterType.Skeleton:
+                    case (int)MonsterType.Goblin:
+
+                        //monster.name
+                        //monster.hp
+                        //monster.attack
+                        break;*/
+                }
+            }
+
+
+            
+
+        }
+    }
+}
