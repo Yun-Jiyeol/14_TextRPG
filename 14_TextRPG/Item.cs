456d6a7f5e87858556acb1d949fb710b9dc8f475
hgkim0728 <hgkim0728@gmail.com>
2025-02-06 19:26:42 +0900

    Feat_아이템 클래서&열거형 접근자 수정

--- 14_TextRPG/Item.cs
+++ 14_TextRPG/Item.cs
@@ -6,19 +6,73 @@ using System.Threading.Tasks;
 
 namespace _14_TextRPG
 {
-    enum ItemType
+    public enum ItemType
     {
-        Weapon = 40,
-        Armor,
+        Weapon = 10,
+        Shield = 20,
+        Armor = 30
     }
-    internal class Item
+    
+    public class Item
     {
         public string Name { get; } //아이템 이름
-        public ItemType Itemtype { get; set; } //아이템 종류
+        public ItemType Itemtype { get; } //아이템 종류
         public string Info { get; } //아이템 설명
-        public int Value { get; set; } //아이템 스텟
-        public int Cost { get; set; } //아이템 가격
+        public int Value { get; } //아이템 스텟
+        public int Cost { get; } //아이템 가격
         public bool PlayerHave { get; set; } //플레이어가 소지 유무
         public bool PlayerUse { get; set; } //플레이어가 착용 유무
+
+        /// <summary>
+        /// 생성자
+        /// 아이템 클래스를 생성할 때 아이템의 정보를 적용
+        /// </summary>
+        /// <param name="_name">아이템 이름</param>
+        /// <param name="_itemType">아이템 종류</param>
+        /// <param name="_itemInfo">아이템 설명</param>
+        /// <param name="_value">아이템 능력치</param>
+        /// <param name="_cost">아이템 가격</param>
+        public Item(string _name, int _itemType, string _itemInfo, int _value, int _cost)
+        {
+            Name = _name;
+            Itemtype = (ItemType)_itemType;
+            Info = _itemInfo;
+            Value = _value;
+            Cost = _cost;
+            PlayerHave = false;
+            PlayerUse = false;
+        }
+
+        public int Buy(int _holdingGold)
+        {
+            if ((!PlayerHave))
+            {
+                if (_holdingGold - Cost >= 0)
+                {
+                    // 인벤토리에 아이템 추가
+                    PlayerHave = true;
+                    Console.WriteLine("구매를 완료했습니다.");
+                    return _holdingGold - Cost;
+                }
+                else
+                {
+                    Console.WriteLine("Gold 가 부족합니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("이미 구매한 아이템입니다.");
+            }
+            
+            return _holdingGold;
+        }
+
+        public void Use()
+        {
+            if(PlayerUse)
+            {
+
+            }
+        }
     }
 }
