e2967d042d85de24a3a9af70e8b776079c7fb3ae
Yun-Jiyeol <yir0612@gmail.com>
2025-02-06 16:45:25 +0900

    Feat_GM에 INPUT추가

--- 14_TextRPG/GameManager.cs
+++ 14_TextRPG/GameManager.cs
@@ -9,5 +9,24 @@ namespace _14_TextRPG
     internal class GameManager
     {
         public void Start() { }
+
+        public int Input(int i) //0-i까지의 숫자를 입력 이외에는 "잘못된 입력입니다."
+        {
+            while (true)
+            {
+                Console.Write("입력 : ");
+                string Pinput = Console.ReadLine();
+                int numinput;
+
+                if (int.TryParse(Pinput, out numinput)) //Pinput => int | 1. true | 2. out numinput = int.Parse(Pinput)
+                {
+                    if (numinput >= 0 && numinput <= i)
+                    {
+                        return numinput;
+                    }
+                }
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
     }
 }
