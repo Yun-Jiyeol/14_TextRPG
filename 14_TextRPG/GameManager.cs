9e2d9264a7345476530864f88f28bec326857277
Yun-Jiyeol <yir0612@gmail.com>
2025-02-06 19:56:37 +0900

    Feat_시작 인터페이스 제작

--- 14_TextRPG/GameManager.cs
+++ 14_TextRPG/GameManager.cs
@@ -8,7 +8,44 @@ namespace _14_TextRPG
 {
     internal class GameManager
     {
-        public void Start() { }
+        public void Start()
+        {
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine();
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine();
+            Console.WriteLine("3. 상점");
+            Console.WriteLine();
+            Console.WriteLine("4. 전투시작");
+            Console.WriteLine();
+            Console.WriteLine("5. 퀘스트");
+            Console.WriteLine();
+            Console.WriteLine("0. 게임 종료");
+            Console.WriteLine();
+
+            int input = Input(5);
+            switch (input)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    //Player.State();
+                    break;
+                case 2:
+                    //Item
+                    break;
+                case 3:
+                    //Store
+                    break;
+                case 4:
+                    //Battle
+                    break;
+                case 5:
+                    //Quest
+                    break;
+            }
+        }
 
         public int Input(int i) //0-i까지의 숫자를 입력 이외에는 "잘못된 입력입니다."
         {
