a5328294129399f12f927a19764183f73e802876
CNyaha <llhb0625@gmail.com>
2025-02-07 09:09:51 +0900

    chore_줄정리2

--- 14_TextRPG/GameManager.cs
+++ 14_TextRPG/GameManager.cs
@@ -8,7 +8,9 @@ namespace _14_TextRPG
 {
     internal class GameManager
     {
+
         public void Start() { }
+
         public int Input(int i) //0-i까지의 숫자를 입력 이외에는 "잘못된 입력입니다."
         {
             while (true)
