3adf84b7720ff1aa3a34dec9655a525590b5ae9c
yeehan12 <matlo2@naver.com>
2025-02-07 13:35:18 +0900

    sj_플레이어 몬스터 상태창

--- 14_TextRPG/Character.cs
+++ 14_TextRPG/Character.cs
@@ -8,8 +8,7 @@ namespace _14_TextRPG
 {
     public class Character
     {
-        GameManager gamemanager;
-        public string Name { get;} //캐릭터 이름
+        public string Name { get; set; } //캐릭터 이름
         public string Class { get; set; } //캐릭터 직업
         public int Level { get; set; } //캐릭터 레벨
         public float Health { get; set; } //캐릭터 현제 체력
@@ -18,8 +17,10 @@ namespace _14_TextRPG
         public int Defence { get; set; } //캐릭터 방어력
         public int Gold { get; set; } //캐릭터 소유 골드
         public bool isDead { get; set; } = false; //캐릭터 사망 유무
+        
         public void Status() //전투 때 보여지게끔 (몬스터)
         {
+            Console.Clear();
             Console.WriteLine($"Lv. {Level}");
             Console.WriteLine($"{Name} ({Class})");
             Console.WriteLine($"공격력 : {Attack} + (0)"); // 아이템으로 오른 공격력 반영 해주세요
@@ -28,17 +29,11 @@ namespace _14_TextRPG
             Console.WriteLine($"Gold : {Gold}G");
             Console.WriteLine("");
             Console.WriteLine("0. 나가기");
-            while (true)
-            { 
-            int exit = int.Parse(Console.ReadLine());
-                switch (exit)
-                { 
-                case 0:
-                        gamemanager.Start();
-                        break;
 
-                }
-            }
+
+            int exit = Input.input(0, 0);
+ 
+            
         }
     }
 }
--- 14_TextRPG/Monster.cs
+++ 14_TextRPG/Monster.cs
@@ -8,6 +8,31 @@ namespace _14_TextRPG
 {
     public class Monster : Character
     {
-
+        public Monster(Character character)
+        {
+            if (character.Level >= 5) //캐릭터의 레벨이 5이하일 때
+            {
+                Name = "슬라임";
+                Health = 100;
+                MaxHealth = 100;
+                Attack = 10;
+            }
+            else if (character.Level >= 10)//캐릭터의 레벨이 10이하일 때
+            {
+                Name = "스톤 골렘";
+                Health = 300;
+                MaxHealth = 300;
+                Attack = 20;
+            }
+            else
+            {
+                ////캐릭터의 레벨이 6 이상, 9이하일 때
+                Name = "초록 버섯";
+                Health = 200;
+                MaxHealth = 200;
+                Attack = 15;
+            }
+        }
+            
     }
 }
