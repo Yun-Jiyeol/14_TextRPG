6989e08d06dbb7b335b5de2b0806a3f3489dd6b9
CNyaha <llhb0625@gmail.com>
2025-02-12 20:28:24 +0900

    docs_commit

--- 14_TextRPG/GameManager.cs
+++ 14_TextRPG/GameManager.cs
@@ -171,11 +171,11 @@ namespace _14_TextRPG
             }
             if (File.Exists(lines[2]))
             {
-                inven = JsonSerializer.Deserialize<Inven>(lines[1]);
+                inven = JsonSerializer.Deserialize<Inven>(lines[2]);
             }
             if (File.Exists(lines[3]))
             {
-                shop = JsonSerializer.Deserialize<Shop>(lines[2]);
+                shop = JsonSerializer.Deserialize<Shop>(lines[3]);
             }
             Start();
         }
